9a398165d0827c2c960b1ca356d5b1408d3f6f67
renatorrocha <renatorrodrigues2002@gmail.com>
2024-09-21 19:51:43 -0300

    feat: env type

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig, type Config } from "drizzle-kit";
+import { env } from "./src/env";
 
 export default defineConfig({
 	schema: "./src/db/schema/index.ts",
 	dialect: "postgresql",
 	out: "./drizzle",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
 }) satisfies Config;

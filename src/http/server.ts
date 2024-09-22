8c7e997524731b6e514d86fdfda5f45d0e977097
renatorrocha <renatorrodrigues2002@gmail.com>
2024-09-22 02:16:18 -0300

    fix(api): body validation /w t

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,37 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { db } from "../db";
 import { restaurants, users } from "../db/schema";
 
-const app = new Elysia().post("/restaurants", async ({ body, set }) => {
-	const { restaurantName, name, email, phone } = body as any;
+const app = new Elysia().post(
+	"/restaurants",
+	async ({ body, set }) => {
+		const { restaurantName, managerName, email, phone } = body;
 
-	const [manager] = await db
-		.insert(users)
-		.values({
-			name,
-			email,
-			phone,
-			role: "manager",
-		})
-		.returning({ id: users.id });
+		const [manager] = await db
+			.insert(users)
+			.values({
+				name: managerName,
+				email,
+				phone,
+				role: "manager",
+			})
+			.returning({ id: users.id });
 
-	await db.insert(restaurants).values({
-		name: restaurantName,
-		managerId: manager.id,
-	});
+		await db.insert(restaurants).values({
+			name: restaurantName,
+			managerId: manager.id,
+		});
 
-	set.status = 204;
-});
+		set.status = 204;
+	},
+	{
+		body: t.Object({
+			restaurantName: t.String(),
+			managerName: t.String(),
+			email: t.String({ format: "email" }),
+			phone: t.String(),
+		}),
+	},
+);
 
 app.listen(3333, () => console.log("HTTP server running on port 3333!"));

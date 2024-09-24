12f0de1f77676d2c84a752fe2b99e61fb4ab9c93
renatorrocha <renatorrodrigues2002@gmail.com>
2024-09-23 22:27:52 -0300

    wip: setting-up jwt

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,8 +2,21 @@ import { Elysia, t } from "elysia";
 import { registerRestaurant } from "./routes/register-restaurant";
 import swagger from "@elysiajs/swagger";
 import { sendAuthLink } from "./routes/send-auth-link";
+import jwt from "@elysiajs/jwt";
+import { env } from "../env";
+import cookie from "@elysiajs/cookie";
 
 const app = new Elysia()
+	.use(
+		jwt({
+			secret: env.JWT_SECRET_KEY,
+			schema: t.Object({
+				sub: t.String(), // Id do usuario
+				restaurantId: t.Optional(t.String()),
+			}),
+		}),
+	)
+	.use(cookie({}))
 	.use(registerRestaurant)
 	.use(sendAuthLink)
 	.use(swagger());
